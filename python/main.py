732c9c8beceffd82201257cde6de00a93bd4a45d
George Wong <wgeorge@fortinet-us.com>
2025-10-15 15:06:15 -0700

    revised

---
+++ python/main.py
@@ -0,0 +1,9 @@
+# CVE-2015-2296
+# CVE-2018-18074
+# CVE-2023-32681
+# WEAKNESS: CVE-2024-35195
+from django.conf import settings
+from django.urls import path, include
+from django.db import models
+from django.views.generic import ListView, DetailView
+
